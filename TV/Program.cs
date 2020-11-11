67d2a4e4e22f97d4e9ab80d211b9e2d47adea094
Marko <marecare006@gmail.com>
2020-11-11 17:12:01 +0100

    PRIMER manipulacije sa klasama i svojstvima

--- TV/Program.cs
+++ TV/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
-
-namespace TV
+﻿namespace TV
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Televizor t1 = new Televizor();
+            
+            
+            t1.pojacanTon();
+            t1.Ukljuci();
+            
+            t1.ispisi();
+            
+            t1.DaLiJeUkljucen();
+            
+            t1.smanjiTon();
+            t1.Iskljuci();
+            
+            t1.Ukljuci();
+            t1.JacinaTona = 50;
+            
+            t1.ispisi();
         }
     }
 }
--- TV/Televizor.cs
+++ TV/Televizor.cs
@@ -1,7 +1,60 @@
+using System;
+
 namespace TV
 {
     public class Televizor
     {
+        public int _jacinaTona = 0;
+        public bool ukljucen = false;
+
+        public int JacinaTona
+        {
+            get => _jacinaTona;
+            set => _jacinaTona = value;
+        }
+
+        public void pojacanTon()
+        {
+            _jacinaTona += 1;
+        }
+
+        public void smanjiTon()
+        {
+            _jacinaTona -= 1;
+        }
+
+        public void Ukljuci()
+        {
+            ukljucen = true;
+        }
+
+        public void Iskljuci()
+        {
+            ukljucen = false;
+        }
+
+        public void DaLiJeUkljucen()
+        {
+            if (ukljucen = true)
+            {
+                Console.WriteLine("Televizor je ukljucen");
+            }
+            else
+            {
+                Console.WriteLine("Televizor je iskljucen");
+            }
+            
+        }
+
+        public void VratiJacinuTona()
+        {
+            Console.WriteLine("Trenutna jacina tona je " + _jacinaTona);
+        }
+
+        public void ispisi()
+        {
+            Console.WriteLine("Televizor je trenutno na jacini " + _jacinaTona + ". Da li je ukljucen? " + ukljucen);
+        }
         
     }
 }
